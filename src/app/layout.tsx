ce7ef280bef49111b94c181788e578037486466b
Favour-Yusuf <favouryusuf45@gmail.com>
2026-05-13 01:20:28 +0100

    error solved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./global.css";
+import "./globals.css";
 import PageTransition from "@/components/PageTransition";
 
 const geistSans = Geist({
